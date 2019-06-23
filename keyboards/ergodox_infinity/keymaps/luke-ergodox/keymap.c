f44f6dec9b439ff42644ea641806d441667238a6
Luke Snape <ldjsnape@gmail.com>
2019-06-23 16:20:11 +0100

    Remap cmd (super)

--- keyboards/ergodox_infinity/keymaps/luke-ergodox/keymap.c
+++ keyboards/ergodox_infinity/keymaps/luke-ergodox/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSLS, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TG(L1),
         KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
         KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(L1),
-        KC_LGUI, KC_GRV, KC_BSLS, KC_LEFT, KC_RGHT,
+        KC_LGUI, KC_GRV, KC_BSLS, KC_LEFT, KC_LGUI,
                                                       KC_LCTL, KC_LALT,
                                                                KC_HOME,
                                              KC_BSPC, KC_DELT, KC_END,
