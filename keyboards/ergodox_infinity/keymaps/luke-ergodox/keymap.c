0b80ce24dc0c1c489b3b14828bb13afe06c4cf77
Luke Snape <lukesnape@Lukes-MBP.lan>
2019-07-08 11:47:38 +0100

    Remap dash and lgui

--- keyboards/ergodox_infinity/keymaps/luke-ergodox/keymap.c
+++ keyboards/ergodox_infinity/keymaps/luke-ergodox/keymap.c
@@ -27,12 +27,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    +-------+-----+-----+-----+-----+-----+ f1  |
  *    | LSHIFT|  Z  |  X  |  C  |  V  |  B  |     |
  *    +-+-----+-----+-----+-----+-----+-----+-----+
- *      |LGUI |  `  |  \  |LEFT |RIGHT|
+ *      |LGUI |  `  |  \  |LEFT |LGUI |
  *      +-----+-----+-----+-----+-----+   +-----+-----+
  *                                        |LCTRL| LALT|
  *                                  +-----+-----+-----+
  *                                  |     |     | HOME|
- *                                  |BKSPC| DEL +-----+
+ *                                  |BKSPC| LGUI+-----+
  *                                  |     |     | END |
  *                                  +-----+-----+-----+
  */
@@ -43,15 +43,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LGUI, KC_GRV, KC_BSLS, KC_LEFT, KC_LGUI,
                                                       KC_LCTL, KC_LALT,
                                                                KC_HOME,
-                                             KC_BSPC, KC_DELT, KC_LALT,
+                                             KC_BSPC, KC_LGUI, KC_LALT,
 /* right hand
  *        +-----+-----+-----+-----+-----+-----+-------+
- *        |LCK-2|  6  |  7  |  8  |  9  |  0  |   -   |
+ *        |LCK-2|  6  |  7  |  8  |  9  |  0  |  f1   |
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        |  [  |  Y  |  U  |  I  |  O  |  P  |   ]   |
  *        |     +-----+-----+-----+-----+-----+-------+
  *        +-----+  H  |  J  |  K  |  L  |  ;  |   '   |
- *        | f1  +-----+-----+-----+-----+-----+-------+
+ *        |  -  +-----+-----+-----+-----+-----+-------+
  *        |     |  N  |  M  |  ,  |  .  |  /  | RSHIFT|
  *        +-----+-----+-----+-----+-----+-----+-----+-+
  *                    |LEFT |DOWN | UP  |RIGHT| RGUI|
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    | ESC |     |     |
  *    +-----+-----+-----+
  */
-             TG(L2),  KC_6,   KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
+             TG(L2),  KC_6,   KC_7,    KC_8,    KC_9,   KC_0,     MO(L1),
              KC_LBRC, KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,     KC_RBRC,
                       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-             MO(L1),  KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_MINS, KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
                               KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_RGUI,
              KC_RALT, KC_RCTL,
              KC_PGUP,
